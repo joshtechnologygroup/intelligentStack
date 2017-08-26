f8ca4e14fe658a3da6a6d7ec5646b2d4d4a80f98
JyotiDhiman <jyoti.dhiman@joshtechnologygroup.com>
2017-08-27 03:57:27 +0530

    fetch question description tags

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def get_questions
     # `python lib/scripts/ordering.py`
-    User.generate_user_performance_data(1,1, 6534673)
+    # User.generate_user_performance_data(1,1, 6534673)
     TaggedQuestion.get_question(6534673)
   end
 end
--- app/models/tagged_question.rb
+++ app/models/tagged_question.rb
@@ -7,21 +7,28 @@ class TaggedQuestion < ApplicationRecord
       unless tag_name
         next
       end
-      `python lib/scripts/ordering.py -t "#{tag_name}"`
+      # `python lib/scripts/ordering.py -t "#{tag_name}"`
       questions = TaggedQuestion.where("tags LIKE '%#{tag_name}%' and accepted_answer_id is NULL")
+      questions.each do |q|
+        question_keywords = User.fetch_question_keywords(q.body).flatten
+        question_keywords = question_keywords.reject {|v| question_keywords.index(v).odd?}
+        question_keywords = valid_question_keywords(question_keywords)
+        if question_keywords.present?
+          q.tags = q.tags.split(',').push(question_keywords).join(',')
+          q.save
+        end
+      end
       unless questions.blank?
         users = questions.map(&:"owner.user_id").uniq
         users.each do |user|
-          User.generate_user_performance_data(1, 1, user)
+          User.generate_user_performance_data(1, 1, user) unless TaggedQuestion.find(user_id: user)
         end
-        query = "drop table if exists q_test1;
-        create table q_test1 as SELECT question_id, link, b.score * 10 + COALESCE(answer_count, 0) * 5 +
+        query = "
+        INSERT INTO suggested_questions SELECT #{user_id}, question_id, link, COALESCE(b.score, 0)* 10 + COALESCE(answer_count, 0) * 5 +
         COALESCE(max_score, 0) * 2 + COALESCE(favorite_count, 0) * 5 + COALESCE(up_vote_count,0)*10
-        - COALESCE(down_vote_count, 0)*100 as sort_column FROM ( select * from questions WHERE (tags LIKE '%ruby%' and accepted_answer_id is NULL) )a JOIN (select * from tag_scores where tag_id ='12') b on a.\"owner.user_id\"=b.user_id::BIGINT WHERE b.score <= #{TagScore.where(tag_id: tag.tag_id,user_id: user_id).first.score} ;
+        - COALESCE(down_vote_count, 0)*100 as sort_column FROM ( select * from questions WHERE (tags LIKE '%ruby%' and accepted_answer_id is NULL) )a JOIN (select * from tag_scores where tag_id ='12') b on a.\"owner.user_id\"=b.user_id::BIGINT WHERE WHERE b.score is NULL or b.score <= #{TagScore.where(tag_id: tag.tag_id,user_id: user_id).first.score} ;
         select * from q_test1 order by sort_column desc;"
-        a = ActiveRecord::Base.connection.execute(query)
-        puts a.inspect
-        puts '*'*100
+        ActiveRecord::Base.connection.execute(query)
         break
       end
     end
