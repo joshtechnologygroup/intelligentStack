292dc326df7db57738b4fbbc32b7215905610516
JyotiDhiman <jyoti.dhiman@joshtechnologygroup.com>
2017-08-27 03:39:13 +0530

    add

--- app/models/tagged_question.rb
+++ app/models/tagged_question.rb
@@ -14,14 +14,16 @@ class TaggedQuestion < ApplicationRecord
         users.each do |user|
           User.generate_user_performance_data(1, 1, user)
         end
+        query = "drop table if exists q_test1;
+        create table q_test1 as SELECT question_id, link, b.score * 10 + COALESCE(answer_count, 0) * 5 +
+        COALESCE(max_score, 0) * 2 + COALESCE(favorite_count, 0) * 5 + COALESCE(up_vote_count,0)*10
+        - COALESCE(down_vote_count, 0)*100 as sort_column FROM ( select * from questions WHERE (tags LIKE '%ruby%' and accepted_answer_id is NULL) )a JOIN (select * from tag_scores where tag_id ='12') b on a.\"owner.user_id\"=b.user_id::BIGINT WHERE b.score <= #{TagScore.where(tag_id: tag.tag_id,user_id: user_id).first.score} ;
+        select * from q_test1 order by sort_column desc;"
+        a = ActiveRecord::Base.connection.execute(query)
+        puts a.inspect
+        puts '*'*100
         break
       end
-      query = "drop table q_test1;
-      create table q_test1 as SELECT question_id, link, b.score * 10 + COALESCE(answer_count, 0) * 5 +
-      COALESCE(max_score, 0) * 2 + COALESCE(favorite_count, 0) * 5 + COALESCE(up_vote_count,0)*10 - COALESCE(down_vote_count, 0)*100 as sort_column FROM ( select * from questions WHERE (tags LIKE '%ruby%' and accepted_answer_id is NULL) )a JOIN (select * from tag_scores where tag_id ='12') b on a.\"owner.user_id\"=b.user_id::BIGINT WHERE b.score <= #{TagScore.where(tag_id: tag.tag_id,user_id: user_id)};
-      select * from q_test1 order by score desc;"
-      a = ActiveRecord::Base.connection.execute(query)
-      puts '*'*100
     end
   end
 end
